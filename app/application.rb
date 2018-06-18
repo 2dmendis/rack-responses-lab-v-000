9e267eead62ab95e4aff262b6b15bd41020f4924
Dmitri Mendis <2dmendis@gmail.com>
2018-06-18 19:54:46 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,3 +7,20 @@ class Application
   end 
   
 end 
+
+class Time 
+  attr_accessor :now
+  def initialize(now)
+    @now == now
+  end 
+  
+  def call(env)
+    resp = Rack::Response.new
+    
+    if @now > 24 
+      resp.write "Good Afternoon!"
+    else 
+      resp.write "Good Morning!"
+    end 
+    resp.finish
+end 
